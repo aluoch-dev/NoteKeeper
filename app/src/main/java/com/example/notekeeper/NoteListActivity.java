be3e35cdb6af082cef8c4f014441dba812691fc7
Laurine Adhiambo <laurinealuoch7@gmail.com>
2020-07-14 14:44:06 +0300

    NoteListActivity

--- app/src/main/java/com/example/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -1,5 +1,6 @@
 package com.example.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
 
@@ -27,5 +33,32 @@ public class NoteListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+        ListView listNotes = findViewById(R.id.list_notes);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,notes);
+
+        listNotes.setAdapter(adapterNotes);
+
+        //handle user clicks
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                //create a new intent instance
+
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+                //start activity and pass intent
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
